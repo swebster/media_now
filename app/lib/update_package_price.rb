d56acaad230c2cc7bc002241d037c96dc300bc9d
Stuart Webster <sw_webster@hotmail.com>
2025-09-17 11:32:04 +0200

    Implement UpdatePackagePrice

--- app/lib/update_package_price.rb
+++ app/lib/update_package_price.rb
@@ -2,6 +2,29 @@
 
 class UpdatePackagePrice
   def self.call(package, new_price_cents, **options)
+    if options.compact.include?(:municipality)
+      update_local(package, options[:municipality], new_price_cents)
+    else
+      update(package, new_price_cents)
+    end
+  end
+
+  private_class_method def self.update_local(package, municipality, new_price_cents)
+    LocalPackage.transaction do
+      # Find or create the relevant local package
+      local_package = LocalPackage.find_or_create_by!(package:, municipality:) do |it|
+        it.amount_cents = package.amount_cents
+      end
+
+      # Add a local pricing history record
+      LocalPrice.create!(local_package:, amount_cents: local_package.amount_cents)
+
+      # Update the current local price
+      local_package.update!(amount_cents: new_price_cents)
+    end
+  end
+
+  private_class_method def self.update(package, new_price_cents)
     Package.transaction do
       # Add a pricing history record
       Price.create!(package: package, amount_cents: package.amount_cents)
--- spec/lib/update_package_price_spec.rb
+++ spec/lib/update_package_price_spec.rb
@@ -1,32 +1,68 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UpdatePackagePrice do
-  it "updates the current price of the provided package" do
-    package = Package.create!(name: "Dunderhonung")
+  let(:honey) { Package.create!(name: 'Dunderhonung', amount_cents: 100_00) }
+  let(:meatballs) { Package.create!(name: 'Farmors köttbullar', amount_cents: 100_00) }
 
-    UpdatePackagePrice.call(package, 200_00)
-    expect(package.reload.amount_cents).to eq(200_00)
+  let(:stockholm) { Municipality.create!(name: 'Stockholm') }
+  let(:gothenburg) { Municipality.create!(name: 'Göteborg') }
+
+  let(:honey_stockholm) do
+    LocalPackage.find_or_create_by!(package: honey, municipality: stockholm) do |it|
+      it.amount_cents = honey.amount_cents
+    end
   end
 
-  it "only updates the passed package price" do
-    package = Package.create!(name: "Dunderhonung")
-    other_package = Package.create!(name: "Farmors köttbullar", amount_cents: 100_00)
+  let(:honey_gothenburg) do
+    LocalPackage.find_or_create_by!(package: honey, municipality: gothenburg) do |it|
+      it.amount_cents = honey.amount_cents
+    end
+  end
 
+  it 'updates the current price of the provided package' do
+    described_class.call(honey, 200_00)
+    expect(honey.reload.amount_cents).to eq(200_00)
+  end
+
+  it 'only updates the passed package price' do
     expect {
-      UpdatePackagePrice.call(package, 200_00)
+      described_class.call(honey, 200_00)
     }.not_to change {
-      other_package.reload.amount_cents
+      meatballs.reload.amount_cents
     }
   end
 
-  it "stores the old price of the provided package in its price history" do
-    package = Package.create!(name: "Dunderhonung", amount_cents: 100_00)
-
-    UpdatePackagePrice.call(package, 200_00)
-    expect(package.prices).to be_one
-    price = package.prices.first
+  it 'stores the old price of the provided package in its price history', :aggregate_failures do
+    described_class.call(honey, 200_00)
+    expect(honey.prices).to be_one
+    price = honey.prices.first
     expect(price.amount_cents).to eq(100_00)
+  end
+
+  it 'updates the current price of the provided local package' do
+    described_class.call(honey, 200_00, municipality: stockholm)
+    expect(honey_stockholm.reload.amount_cents).to eq(200_00)
+  end
+
+  it 'only updates the passed package price for the given municipality' do
+    expect {
+      described_class.call(honey, 200_00, municipality: stockholm)
+    }.not_to change {
+      honey_gothenburg.reload.amount_cents
+    }
+  end
+
+  it 'stores the old price of the provided package for the given municipality', :aggregate_failures do
+    described_class.call(honey, 200_00, municipality: stockholm)
+    expect(honey_stockholm.local_prices).to be_one
+    local_price = honey_stockholm.local_prices.first
+    expect(local_price.amount_cents).to eq(100_00)
+  end
+
+  it 'does not store the old price of the provided package for any other municipality' do
+    described_class.call(honey, 200_00, municipality: stockholm)
+    expect(honey_gothenburg.local_prices).to be_empty
   end
 end
